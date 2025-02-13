8faa35a048e9a930c88bc7b8bcbe4aac1e59d9bc
Aiman A Nawab <nota1mnawab@gmail.com>
2025-02-13 22:43:53 +0530

    Huge changes, considering all students

--- A1m/work_a1m.py
+++ A1m/work_a1m.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import tabula as tb
-
+Date=input('Enter date in the format of DD-MM-YYYY')
+Test_Id=input('Enter test ID :)')
 #================================================================================
 #=========Dividing questions into easy medium and hard===========================
 student_analysis=pd.read_excel("Resources\student_analysis.xls",skiprows=8)
@@ -24,20 +25,8 @@ for i in range(0,60):
         else:
             hard_questions.append(i+1)
 print(easy_questions,med_questions,hard_questions,sep='\n')
-
-#==============================================================================
-#===========Getting info on student's responses================================
-expanded_scorelist=pd.read_excel('Resources\expanded_scorelist.xlsx')
-data2=expanded_scorelist.head(1)
-attempt_correct=list(map(lambda x:int(x),data2["MATHEMATICS R IDS"][0].split(',') ))
-attempt_wrong=list(map(lambda x:int(x),data2["MATHEMATICS W IDS"][0].split(',') ))
-left=list(map(lambda x:int(x),data2["MATHEMATICS L IDS"][0].split(',') ))
-
-
-#==============================================================================
-#=============Blueprint interpretation=========================================
-Mathematics=dict()
 tables = tb.read_pdf(r"Resources\blueprint_data.pdf", pages="1")
+
 blueprint_data = tables[0]
 # Clean column names
 blueprint_data.columns = blueprint_data.columns.str.strip().str.replace(r'\s+', ' ', regex=True)
@@ -59,43 +48,67 @@ for i in range(len(chapterwise_blueprint["Chapter Name"])):
         chapterwise_question_numbers[chapterwise_blueprint["Chapter Name"][i]].append(pointer)
 chapterwise_question_numbers.pop("Total")
 print(chapterwise_question_numbers)
-
 #==============================================================================
-#===========SPI Calculation====================================================
-SPI=0
-for chapter_name,questions in chapterwise_question_numbers.items():
-    if chapter_name in Mathematics:
-        pass
+def making_sure_empty_cells_are_noticed(x):
+    if type(x) != str:
+        return(int(x))
     else:
-        Mathematics[chapter_name]=0
-        for question in questions:
-            if question in attempt_correct:
-                if question in easy_questions:
-                    SPI+=0.8*3
-                elif question in med_questions:
-                    SPI+=1*3
-                else:
-                    SPI+=1.2*3
-            elif question in attempt_wrong:
-                    if i in easy_questions: #MULTIPLIED 3 TO ALL THE WEIGHTS JUST SO THAT THE VALUES ARE BIGGER BECAUSE 
-                        SPI-=0.2*3   #I WAS GETTING single intergers :(
-                    elif i in med_questions:
-                        SPI-=0.15*3
+        return(0)
+#==============================================================================
+#===========Getting info on student's responses================================
+expanded_scorelist=pd.read_excel('Resources\expanded_scorelist.xlsx')
+print('333333333333333333333333333333333333333333',len(expanded_scorelist))
+
+for p in range(0,len(expanded_scorelist)):
+    data2=expanded_scorelist.iloc[p]  
+    attempt_correct=[]
+    attempt_wrong=[]
+    left=[]  
+    attempt_correct=list(map(making_sure_empty_cells_are_noticed,str(data2["MATHEMATICS R IDS"])[0].split(',') ))
+    attempt_wrong=list(map(making_sure_empty_cells_are_noticed,str(data2["MATHEMATICS W IDS"])[0].split(',') ))
+    left=list(map(making_sure_empty_cells_are_noticed,str(data2["MATHEMATICS L IDS"])[0].split(',') ))
+
+
+    #==============================================================================
+    #=============Blueprint interpretation=========================================
+    Mathematics=dict()
+
+    #=============================================================================
+    #===========SPI Calculation====================================================
+    SPI=0
+    for chapter_name,questions in chapterwise_question_numbers.items():
+
+        if chapter_name in Mathematics:
+            pass
+        else:
+            Mathematics[chapter_name]=0
+            for question in questions:
+                if question in attempt_correct:
+                    if question in easy_questions:
+                        SPI+=0.8*3
+                    elif question in med_questions:
+                        SPI+=1*3
                     else:
-                        SPI-=0.1*3
+                        SPI+=1.2*3
+                elif question in attempt_wrong:
+                        if question in easy_questions: #MULTIPLIED 3 TO ALL THE WEIGHTS JUST SO THAT THE VALUES ARE BIGGER BECAUSE 
+                            SPI-=0.2  #I WAS GETTING single intergers :(
+                        elif question in med_questions:
+                            SPI-=0.15
+                        else:
+                            pass
             else:
-                SPI-=len(left)*0.1*3
-        Mathematics[chapter_name]=round(SPI)
-print(Mathematics)
-Date=input('Enter date in the format of DD-MM-YYYY')
-results = []
-for chapter_name, marks_scored in Mathematics.items():
-    results.append([Date, chapter_name, marks_scored])
+                SPI-=len(left)*0.1
+            Mathematics[chapter_name]=round(SPI)
+    print(Mathematics)
+    results = []
+    for chapter_name, marks_scored in Mathematics.items():
+        results.append([Date,expanded_scorelist["CANDIDATE ID"][p],Test_Id, chapter_name, marks_scored])
 
-# Convert to DataFrame
-df = pd.DataFrame(results, columns=["Date", "Chapter_Name", "Marks_Scored"])
+    # Convert to DataFrame
+    df = pd.DataFrame(results, columns=["Date","Roll No","Test ID","Chapter_Name", "Marks_Scored"])
 
-# Save to CSV file
-df.to_csv("Resources/student_scores_save.csv", index=False)
+    # Save to CSV file
+    df.to_csv(f"Data/{expanded_scorelist["CANDIDATE ID"][p]}.csv", index=False)
 
-print("Data saved successfully to student_scores.csv")
+    print("Data saved successfully to student_scores.csv")
