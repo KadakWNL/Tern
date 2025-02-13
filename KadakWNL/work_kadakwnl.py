d4ec25fbc589252b685e7fef8b7c4ffece63b22e
Shreyas Kulkarni <shreyasbackup007@gmail.com>
2025-02-13 22:45:35 +0530

    nothing

--- KadakWNL/work_kadakwnl.py
+++ KadakWNL/work_kadakwnl.py
@@ -1,22 +1,42 @@
 import pandas as pd
 import tabula as tb
 
-# student_analysis=pd.read_excel("Resources\student_analysis.xls",skiprows=8)
-# data=student_analysis.head(68) #Questions start from row 8
-# easy_questions=[]
-# med_questions=[]
-# hard_questions=[]
+#================================================================================
+#=========Dividing questions into easy medium and hard===========================
+student_analysis=pd.read_excel(r"..\Resources\student_analysis.xls",skiprows=8)
+data=student_analysis.head(68) #Questions start from row 8
+easy_questions=[]
+med_questions=[]
+hard_questions=[]
+for i in range(0,60):
+    if data["ATT%"][i]>50:
+        if (data['R%'][i]/data["ATT%"][i])*100>65:
+            easy_questions.append(i+1)
+        elif (data['R%'][i]/data["ATT%"][i])*100<25:
+            hard_questions.append(i+1)
+        else:
+            med_questions.append(i+1)
+    elif data["ATT%"][i]<25:
+        hard_questions.append(i+1)
+    else:
+        if (data['R%'][i]/data["ATT%"][i])*100>65:
+            med_questions.append(i+1)
+        else:
+            hard_questions.append(i+1)
+print(easy_questions,med_questions,hard_questions,sep='\n')
 
-# for i in range(0,60):
-#     if data["ATT%"][i]>50:
-#         if (data['R%'][i]/["ATT%"][i])*100>65:
-#             easy_questions.append[i]
-#         elif (data['R%'][i]/["ATT%"][i])*100<25:
-#             hard_questions.append[i]
-#         else:
-#             med_questions.append[i]
-#     elif data["ATT%"][i]<25:
-#         hard_questions.append[i]
+#==============================================================================
+#===========Getting info on student's responses================================
+expanded_scorelist=pd.read_excel(r'..\Resources\expanded_scorelist.xlsx')
+data2=expanded_scorelist.head(1)
+attempt_correct=list(map(lambda x:int(x),data2["MATHEMATICS R IDS"][0].split(',') ))
+attempt_wrong=list(map(lambda x:int(x),data2["MATHEMATICS W IDS"][0].split(',') ))
+left=list(map(lambda x:int(x),data2["MATHEMATICS L IDS"][0].split(',') ))
+
+
+#==============================================================================
+#=============Blueprint interpretation=========================================
+Mathematics=dict()
 
 # Accessing the blueprint
 tables = tb.read_pdf(r"..\Resources\blueprint_data.pdf", pages="1")
@@ -36,8 +56,9 @@ chapterwise_blueprint = {
 for index, row in blueprint_data.iterrows():
     chapterwise_blueprint["Chapter Name"].append(row["Chapter Name"])
     chapterwise_blueprint["Multiple Choice Question"].append(row["Multiple Choice Question"].split()[0])
-print(chapterwise_blueprint)
+# print(chapterwise_blueprint)
 
+# Stores question numbers based on the blueprint
 chapterwise_question_numbers = {}
 pointer = 0
 for i in range(len(chapterwise_blueprint["Chapter Name"])):
@@ -47,3 +68,5 @@ for i in range(len(chapterwise_blueprint["Chapter Name"])):
         chapterwise_question_numbers[chapterwise_blueprint["Chapter Name"][i]].append(pointer)
 chapterwise_question_numbers.pop("Total")
 print(chapterwise_question_numbers)
+
+
