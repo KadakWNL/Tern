3346c52032b7b0a71c0b02d87862a58e8eab70f7
Shreyas Kulkarni <shreyasbackup007@gmail.com>
2025-02-14 22:31:13 +0530

    GUI DONE FOR THE DAY

--- KadakWNL/work_GUI.py
+++ KadakWNL/work_GUI.py
@@ -54,6 +54,14 @@ test_id_variable = ctk.StringVar()
 test_id_entry = ctk.CTkEntry(frame_right, placeholder_text="000", textvariable=test_id_variable)
 test_id_entry.grid(row=2, column=1, padx=10, pady=(10,10))
 
+#===============================Subject==========================================
+
+subject_entry_variable = ctk.StringVar()
+subject_label = ctk.CTkLabel(frame_right, text="Subject:", font=("Arial", 18))
+subject_label.grid(row=3, column=0, padx=20, pady=(10, 10), sticky="w")
+subject_entry_combobox = ctk.CTkComboBox(frame_right, values=["Phyiscs", "Mathematics", "Chemistry"], variable=subject_entry_variable, font=("Arial", 15), state="readonly")
+subject_entry_combobox.grid(row=3, column=1, padx=10, pady=(10,10))
+
 #=====================All three files upload=====================================
 def open_file(file_label, file_path_var, filetypes):
     file_path = filedialog.askopenfilename(
@@ -63,61 +71,82 @@ def open_file(file_label, file_path_var, filetypes):
     
     if file_path:
         print("File Successfully uploaded")
-        file_label.configure(text=file_path)  # Update the label with file path
+        file_label.configure(text=file_path.split("/")[-1])  # Update the label with file path
         file_path_var.set(file_path)  # Store the file path in the variable
     else:
         print("No file selected")
 
 #=============================================================================================================
 expanded_scorelist_file_label = ctk.CTkLabel(frame_right, text="Expanded Scorelist File (.xlsx): ", font=("Arial", 18))
-expanded_scorelist_file_label.grid(row=3, column=0, padx=20, pady=(10,10), sticky="w")
+expanded_scorelist_file_label.grid(row=4, column=0, padx=20, pady=(10,10), sticky="w")
 
 expanded_scorelist_path = ctk.StringVar()
 
 expanded_scorelist_file_button = ctk.CTkButton(frame_right, text="Select File", font=("Arial", 18),
                                                 command=lambda: open_file(expanded_scorelist_save_flile_label, expanded_scorelist_path, (("Excel Files", "*.xlsx;*.xls"),)))
-expanded_scorelist_file_button.grid(row=3, column=1, padx=10, pady=(10,10))
+expanded_scorelist_file_button.grid(row=4, column=1, padx=10, pady=(10,10))
 
 expanded_scorelist_save_flile_label = ctk.CTkLabel(frame_right, text="Expanded Scorelist Path")
-expanded_scorelist_save_flile_label.grid(row=3, column=2, padx=10, pady=(10,10), sticky="w")
+expanded_scorelist_save_flile_label.grid(row=4, column=2, padx=10, pady=(10,10), sticky="w")
 
 #=============================================================================================================
 student_analysis_file_label = ctk.CTkLabel(frame_right, text="Student Analysis File (.xls): ", font=("Arial", 18))
-student_analysis_file_label.grid(row=4, column=0, padx=20, pady=(10,10), sticky="w")
+student_analysis_file_label.grid(row=5, column=0, padx=20, pady=(10,10), sticky="w")
 
 student_analysis_path = ctk.StringVar()
 
 student_analysis_file_button = ctk.CTkButton(frame_right, text="Select File", font=("Arial", 18),
                                               command=lambda: open_file(student_analysis_save_flile_label, student_analysis_path, (("Excel Files", "*.xls"),)))
-student_analysis_file_button.grid(row=4, column=1, padx=10, pady=(10,10))
+student_analysis_file_button.grid(row=5, column=1, padx=10, pady=(10,10))
 
 student_analysis_save_flile_label = ctk.CTkLabel(frame_right, text="Student Analysis Path")
-student_analysis_save_flile_label.grid(row=4, column=2, padx=10, pady=(10,10), sticky="w")
+student_analysis_save_flile_label.grid(row=5, column=2, padx=10, pady=(10,10), sticky="w")
 
 #=============================================================================================================
 blueprint_data_file_label = ctk.CTkLabel(frame_right, text="Blueprint Data File (.pdf): ", font=("Arial", 18))
-blueprint_data_file_label.grid(row=5, column=0, padx=20, pady=(10,10), sticky="w")
+blueprint_data_file_label.grid(row=6, column=0, padx=20, pady=(10,10), sticky="w")
 
 blueprint_data_path = ctk.StringVar()
 
 blueprint_data_file_button = ctk.CTkButton(frame_right, text="Select File", font=("Arial", 18),
                                             command=lambda: open_file(blueprint_data_save_flile_label, blueprint_data_path, (("PDF Files", "*.pdf"),)))
-blueprint_data_file_button.grid(row=5, column=1, padx=10, pady=(10,10))
+blueprint_data_file_button.grid(row=6, column=1, padx=10, pady=(10,10))
 
 blueprint_data_save_flile_label = ctk.CTkLabel(frame_right, text="Blueprint Data Path")
-blueprint_data_save_flile_label.grid(row=5, column=2, padx=10, pady=(10,10), sticky="w")
+blueprint_data_save_flile_label.grid(row=6, column=2, padx=10, pady=(10,10), sticky="w")
 
 
 #=====================================Button that runs the code====================================================
 #=========================================for the main thing=======================================================
+def empty_variables():
+    test_id_variable.set("")
+    date_entry_variable.set("")
+    subject_entry_variable.set("")
+    expanded_scorelist_path.set("")
+    student_analysis_path.set("")
+    blueprint_data_path.set("")
+    expanded_scorelist_save_flile_label.configure(text="Expanded Scorelist Path")
+    student_analysis_save_flile_label.configure(text="Student Analysis Path")
+    blueprint_data_save_flile_label.configure(text="Blueprint Data Path")
+
 def run_main_code():
     # print(expanded_scorelist_path.get(), student_analysis_path.get(), blueprint_data_path.get())
     if not expanded_scorelist_path.get() or not blueprint_data_path.get() or not student_analysis_path.get():
         messagebox.showerror("File not found", "Some file not uploaded or missing")
         return
+    if not date_entry_variable.get():
+        messagebox.showerror("Input Error", "Please enter a Date before proceeding.")
+        return
+    if not test_id_variable.get():
+        messagebox.showerror("Input Error", "Please enter a Test ID before proceeding.")
+        return
+    if not subject_entry_variable.get():
+        messagebox.showerror("Input Error", "Please enter a Subject before proceeding.")
+        return
     messagebox.showinfo("Test Analysis Report", "Test Analysis Done!")
+    empty_variables()
 
 run_analysis_button = ctk.CTkButton(frame_right, text="Run Test Analysis", width=175, height=40,font=("Arial", 18), command=run_main_code)
-run_analysis_button.grid(row=6, column=1, padx=15, pady=(10,10), sticky="w")
+run_analysis_button.grid(row=7, column=1, padx=15, pady=(10,10), sticky="w")
 
 app.mainloop()
