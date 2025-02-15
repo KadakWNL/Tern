de0f6dee12d44b30819f86bee4d5a62b495d7605
Aiman A Nawab <nota1mnawab@gmail.com>
2025-02-15 11:19:12 +0530

    Added calculation of max from each chapter test_wise

--- A1m/specific_data_calc.py
+++ A1m/specific_data_calc.py
@@ -1,101 +1,221 @@
 import work_a1m
 import pandas as pd
+from pprint import pprint
+from collections import defaultdict
+
+
 test_data=pd.read_csv(r'Data\Metadata\test_metadata.csv')
 DATE_OF_TEST='DATE'
 current_test_id=690 #Get input from user (dashboard)
 find_roll=242007    #could be 'All'
 subject='MATHEMATICS'  #From dashboard
+path_of_data=f'Data/{subject}'
 Subjects=['PHYSICS','CHEMISTRY','MATHEMATICS']
 PHYSICS=[]
 CHEMISTRY=[]
 MATHEMATICS=['Relations and Functions (PUC-I)','Trigonometric Functionsv (PUC-I)','Linear Inequalities (PUC-I)','Limits and Derivatives (PUC-I)',
 'Relations and Functions (PUC-II)','Matrices (PUC-II)','Determinants (PUC-II)','Continuity and Differentiability (PUC-II)']
+
+
+
+
+
+
+
 #1. Average of each chapter from diff tests (tupled with chapter_name)      DONE
 #2. Average of all the tests (avg of 1)                                     DONE
 #3. Average of all students                                                 DONE
 #4. Average of each test (tupled with date and test_id)                     DONE
-#5. Max of a chapter from all the tests. (tupled with date and id)          
+#5. Max of a chapter from all the tests. (tupled with date and id)          DONE
 #6. Max of avg from all chapters. (tupled with date and id)
 #7. Max from 1 test (tupled with date id and chapter name)
-def calculate_average_of_each_chapter_individual(data_by_roll_no):
+
+
+
+
+
+
+
+def calculate_max_marks_in_chapter(roll_no_list, subject, chapter_name, test_data_path):
+    """
+    Finds the maximum marks scored in a specific chapter for each student.
+
+    :param roll_no_list: List of student roll numbers.
+    :param subject: Subject name (e.g., "MATHEMATICS").
+    :param chapter_name: Chapter to find max marks for.
+    :param test_data_path: Path to student test data (folder with CSV files).
+    :return: Dictionary {roll_no: {chapter_name: (max_marks, date, test_id)}}
+    """
+    
+def calculate_max_marks_in_chapters(roll_no_list, subject, chapter_names, test_data_path):
+    """
+    Finds the maximum marks scored in multiple chapters for each student.
+
+    :param roll_no_list: List of student roll numbers.
+    :param subject: Subject name (e.g., "MATHEMATICS").
+    :param chapter_names: List of chapters to find max marks for.
+    :param test_data_path: Path to student test data (folder with CSV files).
+    :return: Dictionary {roll_no: {chapter_name: (max_marks, date, test_id)}}
+    """
+    
+    max_marks_dict = defaultdict(dict)
+
+    for roll in roll_no_list:
+        student_data = {chapter: (0, "N/A", "N/A") for chapter in chapter_names}  # Default to 0 marks
+        
+        try:
+            # Load student's test data
+            data_by_roll_no = pd.read_csv(f"{test_data_path}/{roll}.csv")
+
+            for chapter_name in chapter_names:
+                max_marks = 0  # Default to 0 marks
+                max_date, max_test_id = "N/A", "N/A"
+
+                for index, row in data_by_roll_no.iterrows():
+                    if row['Chapter_Name'] == chapter_name:
+                        marks = row['Marks_Scored']
+                        if marks > max_marks:  # Update if a new max is found
+                            max_marks = marks
+                            max_date = row['Date']
+                            max_test_id = row['Test ID']
+
+                # Store results in dictionary
+                student_data[chapter_name] = (max_marks, max_date, max_test_id)
+
+        except FileNotFoundError:
+            print(f"⚠️ File not found: {test_data_path}/{roll}.csv (Skipping roll {roll})")
+
+        max_marks_dict[int(roll)] = student_data
+
+    return dict(max_marks_dict)
+
+
+def calculate_average_of_each_chapter_individual(data_by_roll_no,roll):
     subjectwise_chapter_average_individual={}
+    subjectwise_chapter_average_individual_roll_wise={}
     temp={}
+
     for chapter_name in eval(subject):
         returning_value=[]
+
         for i in range(len(data_by_roll_no)):
             if data_by_roll_no['Chapter_Name'][i]==chapter_name:
                 returning_value.append(data_by_roll_no['Marks_Scored'][i])
+
         try:
             temp[chapter_name]=round(sum(returning_value)/len(returning_value))
         except ZeroDivisionError:
             pass
+
     subjectwise_chapter_average_individual[subject]=temp
-    return subjectwise_chapter_average_individual
+    subjectwise_chapter_average_individual_roll_wise[int(roll)]=subjectwise_chapter_average_individual
+
+    return subjectwise_chapter_average_individual_roll_wise
 
 
 def main():
+    
     expanded_scorelist=pd.read_excel('Resources/expanded_scorelist.xlsx')
     roll_no=[]
+
     for index in range(2): #replace with: range(len(expanded_scorelist))
         roll_no.append(expanded_scorelist["CANDIDATE ID"][index])
+
     avg_values_all_students=[]
+    avg_values_all_students_rollwise={}
     performance_avg_of_all_students=[]
+
     for roll in roll_no:
+
         performance_avg_of_student={}
         subject_inp=subject
+
         try:
             path_of_data=f'Data/{subject_inp}'
             data_by_roll_no=pd.read_csv(path_of_data+f'/{roll}.csv')
-            value_of_1=calculate_average_of_each_chapter_individual(data_by_roll_no)
+            value_of_1=calculate_average_of_each_chapter_individual(data_by_roll_no,roll)
             avg_values_all_students.append(value_of_1) # THIS IS AVG OF ALL STUDENTS PER CHAPTER
             performance_avg_of_student[subject]=(int(roll),DATE_OF_TEST,round(sum(data_by_roll_no['Marks_Scored'])/(len(data_by_roll_no['Marks_Scored']))))
         except FileNotFoundError:
             performance_avg_of_student[subject]=(int(roll),DATE_OF_TEST,0)
+
         performance_avg_of_all_students.append(performance_avg_of_student)
-    #print(performance_avg_of_all_students)# AVERAGE OF STUDENT IN ALL THE TEST
+
+    #pprint(performance_avg_of_all_students)# AVERAGE OF STUDENT IN ALL THE TEST
+    #pprint(avg_values_all_students)
 
 #==========================================================================
 
     avg_of_whole_class={}
+
     for performance_avg_of_student in performance_avg_of_all_students:
+
         val=[]
         val.append(performance_avg_of_student[subject][2])
         avg_of_whole_class[subject]=(DATE_OF_TEST,round(sum(val)/len(val)))
-    #print(avg_of_whole_class)#  AVERAGE OF WHOLE CLASSSSSSSSSSSSSSS
+
+    #pprint(avg_of_whole_class)#  AVERAGE OF WHOLE CLASSSSSSSSSSSSSSS
 
 #==========================================================================
 
-    class_avg_each_chap={}
-    subject_avg={}
-    for chapter in eval(subject):
-        val=[]
-        for individual_student_avg_dict in avg_values_all_students:
-            val.append(individual_student_avg_dict[subject][chapter])
-        subject_avg[chapter]=round(sum(val)/len(val))
-    class_avg_each_chap[subject]=subject_avg
-    #print(class_avg_each_chap) #THIS IS AVG OF THE CLASS CHAPTERWISE
+    class_avg_each_chap = {} 
+    subject_avg = {} 
+
+    for chapter in eval(subject): 
+        val = []
+        
+        for roll in roll_no: 
+            found = False 
+
+            for individual_student_avg_dict_with_roll in avg_values_all_students:
+                try:
+                    student_data = individual_student_avg_dict_with_roll.get(int(roll), {})
+                    if subject in student_data and chapter in student_data[subject]:
+                        val.append(student_data[subject][chapter])
+                        found = True
+                except KeyError:
+                    pass
+
+            if not found:
+                pass
+
+        if val:
+            subject_avg[chapter] = round(sum(val) / len(val))
+        else:
+            subject_avg[chapter] = 0
+    class_avg_each_chap[subject] = subject_avg.copy()
+    # pprint(class_avg_each_chap)  #Average of the class in each chapters :>
 
 #==========================================================================
     avg_of_each_test={}
+
     for roll in roll_no:
         indiv_avg={}
         val=[]
+
         try:
             path_of_data=f'Data/{subject}'
             data_by_roll_no=pd.read_csv(path_of_data+f'/{roll}.csv')
+
             for index in range(len(data_by_roll_no)):
                 if data_by_roll_no['Test ID'][index]==current_test_id:
                     avg=sum(data_by_roll_no['Marks_Scored'])/len(data_by_roll_no['Marks_Scored'])
                     # val.append(data_by_roll_no['Marks_Scored'])
+
             indiv_avg[current_test_id]=(subject,round(avg))
+
         except ZeroDivisionError:
             indiv_avg[current_test_id]=(subject,0)
-    avg_of_each_test[int(roll)]=indiv_avg
-    #print(avg_of_each_test) #RETURNS AVG OF A TEST GIVEN THE TEST NUMBER WITH IT
+        avg_of_each_test[int(roll)]=indiv_avg
+
+    # pprint(avg_of_each_test) #RETURNS AVG OF A TEST GIVEN THE TEST NUMBER WITH IT
 
 #================================================================================
 
+    max_marks_per_chapter = calculate_max_marks_in_chapters(roll_no, subject, eval(subject), path_of_data)
+    # print(max_marks_per_chapter) #Displays max marks per chapter roll_wise
 
+#================================================================================
 
 # print(calculate_average_of_each_chapter_individual(data_by_roll_no))
 main()
