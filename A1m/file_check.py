35d5e552460578bcb1b2e1bfbaffeb42d6e75726
Aiman A Nawab <nota1mnawab@gmail.com>
2025-02-17 22:53:27 +0530

    Started work on verifying files inputted

---
+++ A1m/file_check.py
@@ -0,0 +1,7 @@
+def main(student_analysis_path,expanded_scorelist_path,blueprint_data_path):
+    pass
+if __name__=='__main__':
+    student_analysis_path=''
+    expanded_scorelist_path=''
+    blueprint_data_path=''
+    main(student_analysis_path,expanded_scorelist_path,blueprint_data_path)
