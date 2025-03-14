a5b455d36e140b807f1fe125358cf76b57913a0d
Aiman A Nawab <nota1mnawab@gmail.com>
2025-03-14 23:30:22 +0530

    Added a loading bar to indicate the process

--- A1m/main.py
+++ A1m/main.py
@@ -2,9 +2,9 @@ import logging
 
 if __name__ == "__main__":
     # Disable all logging messages
-    logging.disable(logging.CRITICAL)
+    logging.disable(logging.ERROR,logging.WARNING,logging.WARNING)
 
     try:
-        import work_GUI
+        import work_GUI_PT2
     except Exception as e:
         pass
